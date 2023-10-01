886f700b506792ef25bd6bf2ed0fdf8e9d0f50fe
владимир назаров <volottnazarov@mail.ru>
2023-10-02 02:00:00 +0300

    создание клиент сервера

---
+++ ClientServer/Server/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это наш сервер");
+            OurServer server = new OurServer();
+        }
+    }
+}
+
